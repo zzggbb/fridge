b3a16ab439a9a5e90a2dede8018af2039875961a
Zane <zbradley96@gmail.com>
2014-05-19 11:02:16 -0700

    Update recipes.js

--- recipes.js
+++ recipes.js
@@ -4,19 +4,12 @@ recipes = [
         "ingredients":  ["eggs","milk","butter","salt","white pepper"],
         "directions":   [
             "1. Crack the eggs into a glass mixing bowl and beat them until they turn a pale yellow color.",
-            "2. Heat a heavy-bottomed nonstick sauté pan over medium-low heat.",
-            "   Add the butter and let it melt.",
-            "3. Add the milk to the eggs and season to taste with salt and white pepper.",
-            "   Then, grab your whisk and whisk like crazy.",
-            "4. When the butter in the pan is hot enough to make a drop of water hiss, pour in the eggs.",
-            "   Don't stir! Let the eggs cook for up to a minute or until the bottom starts to set.",
+            "2. Heat a heavy-bottomed nonstick sauté pan over medium-low heat. Add the butter and let it melt.",
+            "3. Add the milk to the eggs and season to taste with salt and white pepper. Then, grab your whisk and whisk like crazy.",
+            "4. When the butter in the pan is hot enough to make a drop of water hiss, pour in the eggs. Don't stir! Let the eggs cook for up to a minute or until the bottom starts to set.",
             "5. With a heat-resistant rubber spatula, gently push one edge of the egg into the center of the pan, while tilting the pan to allow the still liquid egg to flow in underneath.",
-            "   Repeat with the other edges, until there's no liquid left.",
-            "6. Turn off the heat and continue gently stirring and turning the egg until all the uncooked parts become firm.",
-            "   Don't break up the egg, though. Try to keep the curds as large as possible.",
-            "   If you're adding any other ingredients, now's the time to do it. (See note.)",
-            "7. Transfer to a plate when the eggs are set but still moist and soft.",
-            "   Eggs are delicate, so they'll continue to cook for a few moments after they're on the plate."
+            "6. Turn off the heat and continue gently stirring and turning the egg until all the uncooked parts become firm. Repeat with the other edges, until there's no liquid left. Don't break up the egg, though. Try to keep the curds as large as possible.",
+            "7. Transfer to a plate when the eggs are set but still moist and soft. Eggs are delicate, so they'll continue to cook for a few moments after they're on the plate.",
         ]
     },
     {
@@ -58,8 +51,8 @@ recipes = [
     	    "1. Bring stock to a simmer in a medium saucepan.",
     	    "2. Heat 2 tablespoons oil over medium heat in another saucepan. Cook onion, stirring frequently, until soft, 6 to 7 minutes. Add rice, cook, stirring, until edges are translucent, 2 to 3 minutes. Add wine; cook, stirring, just until evaporated.",
     	    "3. Add 1/2 cup hot stock; cook, stirring, until almost absorbed. Continue adding 1/2 cup stock in this manner until liquid is creamy and rice is al dente, about 20 minutes total (you may not need to add all the stock). Add asparagus with the last addition of stock, and the peas about 1 minute before risotto is done.",
-	        "4. Remove from heat; stir in lemon zest and juice, parsley, cheese, and remaining 2 tablespoons oil. Season with salt and pepper. Serve immediately with additional cheese and lemon zest."
-	    ]
+	    "4. Remove from heat; stir in lemon zest and juice, parsley, cheese, and remaining 2 tablespoons oil. Season with salt and pepper. Serve immediately with additional cheese and lemon zest."
+	]
     },
     {
         "name":         "Escargots Bourguignonne",
@@ -77,6 +70,23 @@ recipes = [
             "2. Melt butter in large skillet. Remove from heat and stir in sugar. Quickly add eggs and beat until well blended. Combine buttermilk with baking soda and stir into mixture in pan. Stir in cornmeal, flour, and salt until well blended and few lumps remain. Pour batter into the prepared pan.",
             "3. Bake in the preheated oven for 30 to 40 minutes, or until a toothpick inserted in the center comes out clean."
         ]   
+    },
+    {
+    	"name":		"Ghormeh Sabzi",
+    	"ingredients":	["beef","onion","oil","tumeric","water","limes","kidney bean","potato","salt","black pepper","green onion","spinach","parsley","cilantro","chives"],
+    	"directions":	[
+    		"1. Trim meat and cut into 3/4 inch cubes",
+    		"2. Fry onion over medium heat in half of the oil until golden",
+    		"3. Add tumeric and fry for 2 more minutes",
+    		"4. Increase heat, add meat cubes and stir over high heat until meat changes color and begins to turn brown",
+    		"5. Reduce heat",
+    		"6. Add water, drained kidney beans, salt and pepper to taste",
+    		"7. Cover and simmer gently for about an hour or until meat is tender",
+    		"8. Fry potatoes over high heat in the remaining "
+    	]
+    },
+    {
+    	
     }
 ]
 
