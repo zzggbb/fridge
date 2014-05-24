6f278f5e69cc62531cca62d22b37ef80a075b243
Zane Bradley <zbradley96@gmail.com>
2014-05-24 03:46:44 -0700

    code aesthetics :bear:

--- input.js
+++ input.js
@@ -1,94 +1,106 @@
 /*
-    purpose:    return input typed into the 'ingredients' input box, false if
-                nothing has been typed. For use in the main function.
+    purpose: Get the users input. For use in the main function.
+    explain: return input typed into the ingredients input box, or false if 
+             nothing was typed.
+    example: 'eggs, cheddar cheese' -> normalize('eggs, cheddar cheese')
+             '' -> false
 */
 function get_input(){
     var ingredients = document.getElementById('input_ingredients').value;
-    if (ingredients){
-        return normalize(ingredients);
-    } else {
-        return false;
-    }
+    return (ingredients ? normalize(ingredients) : false);
 }
-
 /*  
-    purpose:    This function normalizes a wide range of given input, allowing 
-                the user to enter ingredients without concern of the format. 
-    procedure:  Normalize the given string into an array.
-    example:    These example strings will result in ['a','b','c','x','y']
-                    " a,b,c,x,y"
-                    "a, b, c, x, y"
+    purpose: This function normalizes a wide range of given input, allowing 
+             the user to enter ingredients without concern of the format. 
+    explain: Normalize the given string into an array.
+    example: These example strings will result in ['a','b','c','x','y']
+                " a,b,c,x,y"
+                "a, b, c, x, y"
 */
 function normalize(string){
     return string.toLowerCase().trim().replace(/ *, */g,",").split(",");
 }
-
 /*
-    purpose:    return number of common elements between two recipe lists.
-    procedure:  filters list `a` by checking if each item in `a` exists in `b`, 
-                and then returns the length of that filtered list.
-    example:    common([1,2,3],[2,3,4]) -> [2,3].length -> 2
-                Therefore, 2 common elements.
+    purpose: return number of common elements between two recipe lists.
+    explain: filters list `a` by checking if each item in `a` exists in `b`, 
+             and then returns the length of that filtered list.
+    example: common([1,2,3],[2,3,4]) -> [2,3].length -> 2
+             Therefore, 2 common elements.
 */
 function common(a, b){
     return a.filter(function(n){ 
         return b.indexOf(n) != -1;
     }).length;
 }
-function clear_elements(element_names){
-    element_names.map(function(x){
-        var element = document.getElementById(x);
-        if (element == null){return;}
-        while (element.firstChild){
-            element.removeChild(element.firstChild);
+/*
+    purpose: simplify the process of clearing all child elements from a given
+             parent element. 
+    explain: for each child in each parent of the list of parents passed as an 
+             argument, remove each of its children until there are none left.
+*/
+function clear_elements(parents){
+    parents.map(function(x){
+        var child = document.getElementById(x);
+        if (child == null){return;}
+        while (child.firstChild){
+            child.removeChild(child.firstChild);
         }
     });
 }
-
+/*
+    purpose: append an error the the `errors` element
+    explain: match the passed `type` argument to an error message in the 
+             `error_map` object, and append the message to the `errors` div.
+*/
 function add_error(type) {
     var error = document.createElement('div');
     var error_map = {
-        'bad_match': "No matches found for the provided ingredients, we're working on it!",
+        'bad_match': "No matches found, check your spelling, or maybe we need more recipes...",
         'bad_input': "Please type a list of comma separated ingredients",
     }
     error.id=type;
     error.innerHTML=error_map[type];
     document.getElementById('errors').appendChild(error);
-    
 }
-
+/*
+    purpose: append a formatted `recipe` element to the visible list of 
+             recipes. 
+    explain: create an object with `div` elements and their ids as keys. set 
+             their `innerHTML` appropriately and append each element in the
+             object to the `recipe_outer` div.
+*/
 function add_recipe(recipe){
-    var recipe_wrap       = document.createElement('div');    
-    var name              = document.createElement('div');
-    var ingredients       = document.createElement('div');
-    var recipe_directions = document.createElement('div');
-    
-    recipe_wrap.id       = 'recipe_wrap';
-    name.id              = 'recipe_name';
-    ingredients.id       = 'recipe_ingredients';
-    recipe_directions.id = 'recipe_directions';
-    
-    name.innerHTML = recipe['name'];
-    ingredients.innerHTML = recipe['ingredients'].join(", ");
-    
+    var elems = {};
+    ['wrap','name','ingredients','directions'].map(function(item){
+        var elem = document.createElement('div');
+        elem.id = 'recipe_' + item;
+        elems[item] = elem;
+    });
+    elems['name'].innerHTML = recipe['name'];
+    elems['ingredients'].innerHTML = recipe['ingredients'].join(", ");
     recipe['directions'].map(function(item){
         var direction_line = document.createElement('div');
         direction_line.id = 'direction_line';
-        direction_line.innerHTML = item;
-        recipe_directions.appendChild(direction_line);       
+        var direction_num = recipe['directions'].indexOf(item) + 1
+        direction_line.innerHTML = direction_num + ". " + item;
+        elems['directions'].appendChild(direction_line);       
     });
-    
-    [name,ingredients,recipe_directions].map(function(item){
-        recipe_wrap.appendChild(item);
+    Object.keys(elems).slice(1).map(function(item){
+        elems['wrap'].appendChild(elems[item]);
     });
-
-    document.getElementById('recipe_outer').appendChild(recipe_wrap);
+    document.getElementById('recipe_outer').appendChild(elems['wrap']);
 }
-
+/*
+    purpose: main function, initiate everything when the user click the 
+             `Submit Ingredients` button.
+    explain: get the input, clear all existing errors, sort recipes based on 
+             the typed ingredients if get_input returns true, otherwise give an
+             error message.
+*/
 function main(){
     var input = get_input();
+    clear_elements(['errors','recipe_outer']);
     if(input){
-        clear_elements(['errors','recipe_outer']);
         recipes.sort(function(a,b){
         /*
             return a comparison value (-,0,+) based on the commonalities between
@@ -99,12 +111,16 @@ function main(){
             the return value of this function is used as the key to sort the 
             elements of the recipes.
         */
-            return common(input,b["ingredients"])-common(input,a["ingredients"]);
+            return common(input,b['ingredients'])-common(input,a['ingredients']);
         });
         if ( common(input,recipes[0]['ingredients']) == 0 ){
             // not even the best matched
             add_error('bad_match');
         } else {
+            /*
+                slice the top three, filter out those that do not have more 
+                than 0, and send what remains to `add_recipe`.
+            */
             recipes.slice(0,3).filter(function(n){
                 return common(input,n['ingredients']) > 0
             }).map(add_recipe); 
--- recipes.js
+++ recipes.js
@@ -3,33 +3,33 @@ recipes = [
         "name":         "Scrambled Eggs",
         "ingredients":  ["eggs","milk","butter","salt","white pepper"],
         "directions":   [
-            "1. Crack the eggs into a glass mixing bowl and beat them until they turn a pale yellow color.",
-            "2. Heat a heavy-bottomed nonstick sauté pan over medium-low heat. Add the butter and let it melt.",
-            "3. Add the milk to the eggs and season to taste with salt and white pepper. Then, grab your whisk and whisk like crazy.",
-            "4. When the butter in the pan is hot enough to make a drop of water hiss, pour in the eggs. Don't stir! Let the eggs cook for up to a minute or until the bottom starts to set.",
-            "5. With a heat-resistant rubber spatula, gently push one edge of the egg into the center of the pan, while tilting the pan to allow the still liquid egg to flow in underneath.",
-            "6. Turn off the heat and continue gently stirring and turning the egg until all the uncooked parts become firm. Repeat with the other edges, until there's no liquid left. Don't break up the egg, though. Try to keep the curds as large as possible.",
-            "7. Transfer to a plate when the eggs are set but still moist and soft. Eggs are delicate, so they'll continue to cook for a few moments after they're on the plate.",
+            "Crack the eggs into a glass mixing bowl and beat them until they turn a pale yellow color.",
+            "Heat a heavy-bottomed nonstick sauté pan over medium-low heat. Add the butter and let it melt.",
+            "Add the milk to the eggs and season to taste with salt and white pepper. Then, grab your whisk and whisk like crazy.",
+            "When the butter in the pan is hot enough to make a drop of water hiss, pour in the eggs. Don't stir! Let the eggs cook for up to a minute or until the bottom starts to set.",
+            "With a heat-resistant rubber spatula, gently push one edge of the egg into the center of the pan, while tilting the pan to allow the still liquid egg to flow in underneath.",
+            "Turn off the heat and continue gently stirring and turning the egg until all the uncooked parts become firm. Repeat with the other edges, until there's no liquid left. Don't break up the egg, though. Try to keep the curds as large as possible.",
+            "Transfer to a plate when the eggs are set but still moist and soft. Eggs are delicate, so they'll continue to cook for a few moments after they're on the plate.",
         ]
     },
     {
         "name":         "Pancakes",
         "ingredients":  ["eggs","butter","salt","milk","flour","baking powder","sugar"], 
         "directions":   [
-            "1. In a large bowl, sift together the flour, baking powder, salt and sugar.",
-            "2. Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.",
-            "3. Heat a lightly oiled griddle or frying pan over medium high heat.",
-            "4. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake.",
-            "5. Brown on both sides and serve hot."
+            "In a large bowl, sift together the flour, baking powder, salt and sugar.",
+            "Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.",
+            "Heat a lightly oiled griddle or frying pan over medium high heat.",
+            "Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake.",
+            "Brown on both sides and serve hot."
         ]
     },
     {
         "name":         "Cat",
     	"ingredients":  ["marmalade","parsley","butter","ginger"],
     	"directions":   [
-    		"1. In a large bowl, mix the marmalde, parsley, and butter together.",
-    		"2. Blend in the cat using a grinder.",
-    		"3. Serve hot!."
+    		"In a large bowl, mix the marmalde, parsley, and butter together.",
+    		"Blend in the cat using a grinder.",
+    		"Serve hot!."
     	
     	]
     },
@@ -37,58 +37,114 @@ recipes = [
         "name":         "Beef Curry",
     	"ingredients":  ["coconut milk","red curry paste","chicken broth","fish sauce","brown sugar","red bell pepper","asparagus","sirloin steak","rice noodles"],
     	"directions":   [
-    		"1. In a large wok or skillet, heat 1/2 cup coconut milk over medium-high, stirring, until shiny and thick, 2 minutes.",
-    		"2. Add curry paste and cook, stirring, until fragrant, 1 minute.",
-    		"3. Add remaining coconut milk, broth, fish sauce, and brown sugar and bring to a simmer. ",
-    		"4. Add bell pepper and cook until softened, 2 minutes. Add asparagus and cook until crisp-tender, 3 minutes. ",
-    		"5. Add steak and cook, stirring, until just cooked through, 2 minutes. Serve over noodles with lime wedges and cilantro."
+    		"In a large wok or skillet, heat 1/2 cup coconut milk over medium-high, stirring, until shiny and thick, 2 minutes.",
+    		"Add curry paste and cook, stirring, until fragrant, 1 minute.",
+    		"Add remaining coconut milk, broth, fish sauce, and brown sugar and bring to a simmer. ",
+    		"Add bell pepper and cook until softened, 2 minutes. Add asparagus and cook until crisp-tender, 3 minutes. ",
+    		"Add steak and cook, stirring, until just cooked through, 2 minutes. Serve over noodles with lime wedges and cilantro."
     	]  
     },
     {
         "name":         "Asparagus and Lemon Risotto ",
     	"ingredients" : ["vegetable broth","olive oil","onion","rice","white wine","asparagus","peas","lemon juice","parsley"],
     	"directions":   [
-    	    "1. Bring stock to a simmer in a medium saucepan.",
-    	    "2. Heat 2 tablespoons oil over medium heat in another saucepan. Cook onion, stirring frequently, until soft, 6 to 7 minutes. Add rice, cook, stirring, until edges are translucent, 2 to 3 minutes. Add wine; cook, stirring, just until evaporated.",
-    	    "3. Add 1/2 cup hot stock; cook, stirring, until almost absorbed. Continue adding 1/2 cup stock in this manner until liquid is creamy and rice is al dente, about 20 minutes total (you may not need to add all the stock). Add asparagus with the last addition of stock, and the peas about 1 minute before risotto is done.",
-	    "4. Remove from heat; stir in lemon zest and juice, parsley, cheese, and remaining 2 tablespoons oil. Season with salt and pepper. Serve immediately with additional cheese and lemon zest."
+    	    "Bring stock to a simmer in a medium saucepan.",
+    	    "Heat 2 tablespoons oil over medium heat in another saucepan. Cook onion, stirring frequently, until soft, 6 to 7 minutes. Add rice, cook, stirring, until edges are translucent, 2 to 3 minutes. Add wine; cook, stirring, just until evaporated.",
+    	    "Add 1/2 cup hot stock; cook, stirring, until almost absorbed. Continue adding 1/2 cup stock in this manner until liquid is creamy and rice is al dente, about 20 minutes total (you may not need to add all the stock). Add asparagus with the last addition of stock, and the peas about 1 minute before risotto is done.",
+	    "Remove from heat; stir in lemon zest and juice, parsley, cheese, and remaining 2 tablespoons oil. Season with salt and pepper. Serve immediately with additional cheese and lemon zest."
 	]
     },
     {
         "name":         "Escargots Bourguignonne",
         "ingredients":  ["butter","parsley","white wine","cognac","shallot","salt","black pepper","nutmeg","snails","bread"],
         "directions":   [
-            "1. In a bowl, whisk together butter, parsley, wine, cognac, garlic, and shallots with a fork. Season with salt,pepper, and nutmeg. Cover with plastic wrap and refrigerate overnight to let the ﬂavors meld.",
-            "2. Heat oven to 400°. Spoon about ½ tsp. of butter mixture into each snail shell. Push a snail into each shell; ﬁll shells with remaining butter mixture. Cover bottom of a 9 x 13 baking pan with a layer of rock salt. Arrange snail shells butter side up on bed of salt and bake until butter sizzles, 10–12 minutes. Serve snails on a platter, with bread to soak up the butter, if you like.",
+            "In a bowl, whisk together butter, parsley, wine, cognac, garlic, and shallots with a fork. Season with salt,pepper, and nutmeg. Cover with plastic wrap and refrigerate overnight to let the ﬂavors meld.",
+            "Heat oven to 400°. Spoon about ½ tsp. of butter mixture into each snail shell. Push a snail into each shell; ﬁll shells with remaining butter mixture. Cover bottom of a 9 x 13 baking pan with a layer of rock salt. Arrange snail shells butter side up on bed of salt and bake until butter sizzles, 10–12 minutes. Serve snails on a platter, with bread to soak up the butter, if you like.",
         ]
     },
     {
         "name":         "Grandmother's Buttermilk Cornbread",
         "ingredients":  ["butter","white sugar","eggs","buttermilk","baking soda","cornmeal","flour","salt"],
         "directions":   [
-            "1. Preheat oven to 375 degrees F (175 degrees C). Grease an 8 inch square pan.",
-            "2. Melt butter in large skillet. Remove from heat and stir in sugar. Quickly add eggs and beat until well blended. Combine buttermilk with baking soda and stir into mixture in pan. Stir in cornmeal, flour, and salt until well blended and few lumps remain. Pour batter into the prepared pan.",
-            "3. Bake in the preheated oven for 30 to 40 minutes, or until a toothpick inserted in the center comes out clean."
+            "Preheat oven to 375 degrees F (175 degrees C). Grease an 8 inch square pan.",
+            "Melt butter in large skillet. Remove from heat and stir in sugar. Quickly add eggs and beat until well blended. Combine buttermilk with baking soda and stir into mixture in pan. Stir in cornmeal, flour, and salt until well blended and few lumps remain. Pour batter into the prepared pan.",
+            "Bake in the preheated oven for 30 to 40 minutes, or until a toothpick inserted in the center comes out clean."
         ]   
     },
     {
-    	"name":		"Ghormeh Sabzi",
+    	"name":		    "Ghormeh Sabzi",
     	"ingredients":	["beef","onion","oil","tumeric","water","limes","kidney bean","potato","salt","black pepper","green onion","spinach","parsley","cilantro","chives"],
     	"directions":	[
-    		"1. Trim meat and cut into 3/4 inch cubes",
-    		"2. Fry onion over medium heat in half of the oil until golden",
-    		"3. Add tumeric and fry for 2 more minutes",
-    		"4. Increase heat, add meat cubes and stir over high heat until meat changes color and begins to turn brown",
-    		"5. Reduce heat",
-    		"6. Add water, drained kidney beans, salt and pepper to taste",
-    		"7. Cover and simmer gently for about an hour or until meat is tender",
-    		"8. Fry potatoes over high heat in the remaining "
+    		"Trim meat and cut into 3/4 inch cubes",
+    		"Fry onion over medium heat in half of the oil until golden",
+    		"Add tumeric and fry for 2 more minutes",
+    		"Increase heat, add meat cubes and stir over high heat until meat changes color and begins to turn brown",
+    		"Reduce heat",
+    		"Add water, drained kidney beans, salt and pepper to taste",
+    		"Cover and simmer gently for about an hour or until meat is tender",
+    		"Fry potatoes over high heat in the remaining "
     	]
     },
     {
     	"name":			"Cream Cheese and Tomato Omelette",
-    	"ingredients":	[''],
-    	"directions":['']
+    	"ingredients":	['eggs','salt','black pepper','cream cheese','tomatoes','chives'],
+    	"directions":   [
+            "Whisk eggs, milk, salt, and pepper together in a bowl.",
+            "Heat a 6-inch nonstick skillet over medium heat; pour egg mixture into the hot skillet, tilting so egg mixture covers the entire bottom of skillet. Slowly cook egg mixture until set, 5 to 10 minutes.",
+            "Arrange small dollops of cream cheese onto half the omelet; sprinkle tomato and chives over cream cheese. Fold omelet in half over the fillings. Remove skillet from heat and cover until cream cheese has softened, 2 to 3 minutes."    
+        ]
+    },
+    {
+        "name":         "Brown Butter Raspberry Tart",
+        "ingredients":  ['butter','sugar','vanilla extract','flour','salt','eggs','raspberries'],
+        "directions":   [
+            "Position rack in center of oven and preheat to 375°F. Using rubber spatula or fork, mix melted butter, sugar, and vanilla in medium bowl. Add flour and salt and stir until incorporated. Transfer dough to 9-inch-diameter tart pan with removable bottom. Using fingertips, press dough evenly onto sides and bottom of pan.",
+            "Bake crust until golden, about 18 minutes (crust will puff slightly while baking). Transfer crust to rack and cool in pan. Maintain oven temperature.",
+            "Whisk sugar, eggs, and salt in medium bowl to blend. Add flour and vanilla; whisk until smooth. Cook butter in heavy small saucepan over medium heat until deep nutty brown (do not burn), stirring often, about 6 minutes. Immediately pour browned butter into glass measuring cup. Gradually whisk browned butter into sugar-egg mixture; whisk until well blended.",
+            "Arrange raspberries, pointed side up and close together in concentric circles, in bottom of cooled crust. Carefully pour browned butter mixture evenly over berries. Place tart on rimmed baking sheet. Bake tart until filling is puffed and golden and tester inserted into center comes out clean, about 40 minutes. Cool tart completely in pan on rack.",
+            "Remove tart pan sides. Place tart on platter. Cut into wedges and serve."
+        ]
+    },
+    {
+        "name":         "Grilled Cheese with Tomato, Peppers, and Basil",
+        "ingredients":  ['bread','cheddar cheese','tomato','peppers','basil','salt','pepper','butter'],
+        "directions":   [
+            "Butter one side of each slice of bread, and place 4 of the slices butter-side down on a griddle over medium heat.",
+            "On each piece of bread, place one slice of cheese, 1 slice of tomato, and a few slices of serrano pepper. Sprinkle dried basil and salt and pepper to taste. Top each sandwich with a slice of buttered bread, butter-side up.",
+            "Grill sandwiches until golden brown, about 2 to 3 minutes each side."  
+        ]
+    },
+    {
+        "name":         "Beef Enchiladas",
+        "ingredients":  ['beef','onions','enchilada sauce','tortillas','cheddar cheese','black olives'],
+        "directions":   [
+            "Preheat oven to 350 degrees F (175 degrees C). In a medium skillet over medium high heat, cook the ground beef and onion until beef is evenly browned and onion is tender.",
+            "Prepare the enchilada sauce according to package directions. Pour 1/4 cup of the sauce into the bottom of a 9x13 inch baking dish.",
+            "On each flour tortilla, place an equal portion of the ground beef mixture and about 1 ounce of Cheddar cheese, reserving at least 1/2 cup of cheese. Then tightly roll the tortillas and place seam side down in the baking dish.",
+            "Pour the remaining sauce over the top of the enchiladas and sprinkle with the remaining cheese and olives.",
+            "Bake in a preheated oven for 20 minutes, or until the sauce is bubbly and cheese is thoroughly melted."
+        ]
+    },
+    {
+        "name":         "Strawberry Crepes",
+        "ingredients":  ['eggs','milk','water','butter','flour','salt','cream cheese','sugar','lemon juice','lemon zest','vanilla','whipped cream','strawberries'],
+        "directions":   [
+            "Place the eggs, milk, water, melted butter, flour, and salt in the pitcher of a blender; blend until smooth.",
+            "Heat a lightly oiled griddle or non-stick skillet over medium heat. Pour or scoop the batter onto the griddle, using approximately 2 tablespoons for each crepe.",
+            "Tip and rotate pan to spread batter as thinly as possible. Flip over when the batter is set and the edges are beginning to brown. Cook until the other side begins to brown. Stack finished crepes on a plate, cover with a damp towel and set aside.",
+            "Blend the cream cheese, confectioners' sugar, lemon juice, lemon zest, and vanilla with an electric mixer until smooth. Gently fold in the whipped cream.",
+            "To serve, fill each crepe with 1/4 cup sliced strawberries and 1/3 cup of the cream cheese filling, roll up and top with a small dollop of the cream cheese filling and more sliced strawberries."
+        ]
+    },
+    {
+        "name":         "Banana Bread",
+        "ingredients":  ['flour','baking soda','salt','butter','brown sugar','eggs','bananas'],
+        "directions":   [
+            "Preheat oven to 350 degrees F (175 degrees C). Lightly grease a 9x5 inch loaf pan.",
+            "In a large bowl, combine flour, baking soda and salt. In a separate bowl, cream together butter and brown sugar. Stir in eggs and mashed bananas until well blended.",
+            "Stir banana mixture into flour mixture; stir just to moisten. Pour batter into prepared loaf pan.",
+            "Bake in preheated oven for 60 to 65 minutes, until a toothpick inserted into center of the loaf comes out clean. Let bread cool in pan for 10 minutes, then turn out onto a wire rack."
+        ]  
     }
 ]
 
