0d2cb7a0a682a5ef6c7dc8bd97a2b4f96bb1a10e
Zane Bradley <zbradley96@gmail.com>
2014-05-27 11:21:58 -0700

    recipes error

--- recipes.js
+++ recipes.js
@@ -174,7 +174,7 @@ recipes = [
         "ingredients":  ['beef', 'black pepper', 'lime', 'tomatillos', 'canola oil', 'beef broth', 'corn tortillas', 'tomatos', 'onion', 'avocado', 'lemón'],
         "directions":   [
             "Place sliced meat into a shallow bowl, and season with salt, black pepper, and crushed red pepper. Squeeze the lime juice over the meat, and turn until evenly coated. Cover, and refrigerate for 30 minutes.",
-            "In a blender or food processor, combine tomatillo and jalapeno. Puree for 15 to 20 seconds, or until thick. Heat 1 tablespoon oil in a large skillet over medium high heat. 
+            "In a blender or food processor, combine tomatillo and jalapeno. Puree for 15 to 20 seconds, or until thick. Heat 1 tablespoon oil in a large skillet over medium high heat.", 
             "Carefully pour in tomatillo mixture. Cook, stirring frequently, for 5 minutes. Stir in beef broth. Reduce heat, and simmer for 20 to 30 minutes, or until mixture coats a spoon. Transfer mixture to a serving dish.",
             "Heat tablespoon oil in a large skillet over high heat. Stir in 1/3 of the beef, and saute for 1 minute. Transfer to serving dish.",
             "Repeat with remaining beef. Meanwhile, heat tortillas in the oven or microwave, according to package instructions.",
